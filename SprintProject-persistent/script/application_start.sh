6c2f0ba1a161d418034db7492874fb1420e5685a
Andrés Felipe  López Ochoa <anflopezoc@gmail.com>
2022-01-06 17:42:16 -0500

    CodeDeploy

--- SprintProject-persistent/script/application_start.sh
+++ SprintProject-persistent/script/application_start.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ubuntu/sprintproject3/SprintProject-persistent
-
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/sprintproject3/SprintProject-persistent
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
 
 #start our node app in the background
-node src/server.js > src/server.out.log 2> src/server.err.log < /dev/null & 
+pm2 start ecosystem.config.js --env local --watch
