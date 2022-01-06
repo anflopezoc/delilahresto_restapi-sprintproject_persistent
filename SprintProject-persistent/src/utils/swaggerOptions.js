261b699ce3fb1e147bc5beb4f38b89e61b8a7f2a
Andrés Felipe  López Ochoa <anflopezoc@gmail.com>
2022-01-06 17:45:01 -0500

    Documentation Update

--- SprintProject-persistent/src/utils/swaggerOptions.js
+++ SprintProject-persistent/src/utils/swaggerOptions.js
@@ -4,7 +4,7 @@ const swaggerOptions = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "REST API Delilah Restó- Sprint Project 2 Acámica with DB",
+            title: "REST API Delilah Restó- Sprint Project 2 Acámica with DB and AWS's Cloud",
             version: "2.0.0",
             description: "REST API for orders of Delilah Restó users made with Node JS and MariaDB in AWS's cloud"
         },
