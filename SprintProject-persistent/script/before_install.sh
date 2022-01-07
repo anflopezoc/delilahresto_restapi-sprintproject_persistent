10ca46245e36206e2721382edee38972bf872c91
anflopezoc <anflopezoc@gmail.com>
2022-01-06 22:51:45 -0500

    Scripts Update

--- SprintProject-persistent/script/before_install.sh
+++ SprintProject-persistent/script/before_install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
 
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/sprintproject3/SprintProject-persistent"
+
 if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
+    echo "$DIR exists"
+    rm -rf ${DIR}
+#    rm -rf ${DIR}/{*,.*}
 fi
+
+mkdir ${DIR}
+
+cd ${DIR}
+
+cp ../tmp/.env .env
