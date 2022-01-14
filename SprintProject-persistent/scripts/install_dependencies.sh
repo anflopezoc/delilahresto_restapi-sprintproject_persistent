4aa234aa3634d0a6ace8c6afd107a65fcb7c21cc
anflopezoc <anflopezoc@gmail.com>
2022-01-13 21:25:50 -0500

    scripts v7

--- SprintProject-persistent/scripts/install_dependencies.sh
+++ SprintProject-persistent/scripts/install_dependencies.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo apt-get install httpd
+sudo apt-get install apache2
+
+sudo npm install
+
+pm2 start /home/ubuntu/sprintproject3/SprintProject-persistent/ecosystem.config.js --env production -watch
