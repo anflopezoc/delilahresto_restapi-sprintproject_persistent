9499c21da05f230146cef140f5bbea6c8a039335
anflopezoc <anflopezoc@gmail.com>
2022-01-15 10:57:28 -0500

    before install fixed test

--- SprintProject-persistent/scripts/before_Install.sh
+++ SprintProject-persistent/scripts/before_Install.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 DIR="/home/ubuntu/sprintproject3/SprintProject-persistent"
 
 if [ -d "$DIR" ]; then
     echo "$DIR exists"
     rm -rf ${DIR}
-#    rm -rf ${DIR}/{*,.*}
 fi
 
 mkdir ${DIR}
