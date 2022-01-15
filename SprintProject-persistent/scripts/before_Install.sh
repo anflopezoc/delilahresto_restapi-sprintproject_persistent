e083b564437fcfe0b994cdd4de193862d644f093
anflopezoc <anflopezoc@gmail.com>
2022-01-14 23:01:54 -0500

    scripts v2

--- SprintProject-persistent/scripts/before_Install.sh
+++ SprintProject-persistent/scripts/before_Install.sh
@@ -2,11 +2,7 @@
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/sprintproject3/SprintProject-persistent"
 
-if [ -d "$DIR" ]; then
-    echo "$DIR exists"
-    rm -rf ${DIR}
-#    rm -rf ${DIR}/{*,.*}
-fi
+rm -rf ${DIR}
 
 mkdir ${DIR}
 
