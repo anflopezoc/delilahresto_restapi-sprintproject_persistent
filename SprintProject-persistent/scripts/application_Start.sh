d3fa2dec3a92365fd3579eaeae2d9d8b10baabea
anflopezoc <anflopezoc@gmail.com>
2022-01-20 16:42:38 -0500

    script app start

--- SprintProject-persistent/scripts/application_Start.sh
+++ SprintProject-persistent/scripts/application_Start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+sudo chmod -R 777 /home/ubuntu/sprintproject3/SprintProject-persistent
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/sprintproject3/SprintProject-persistent
 
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
 #install node modules
-sudo npm install
+npm install
 
 #start our node app in the background
 pm2 start ecosystem.config.js --env production --watch
