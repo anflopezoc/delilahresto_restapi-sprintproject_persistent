552e6dcf7aad2afe6ad32e96dae9cab9ad6e732e
Andrés Felipe  López Ochoa <anflopezoc@gmail.com>
2022-01-23 01:17:02 -0500

    Scripts Fixed unix format v2

--- SprintProject-persistent/scripts/application_Start.sh
+++ SprintProject-persistent/scripts/application_Start.sh
@@ -3,15 +3,10 @@
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/sprintproject3/SprintProject-persistent
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
 #install node modules
 sudo npm install
 echo "node modules installed"
 #start our node app in the background
 
-pm2 start ecosystem.config.js --env production --watch
+sudo pm2 start ecosystem.config.js --env production --watch
 echo "run pm2"
