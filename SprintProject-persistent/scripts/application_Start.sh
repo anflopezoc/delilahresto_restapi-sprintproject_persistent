6b6d4e24bbe4182d138b374e097fd38ae99d0b4e
Andrés Felipe  López Ochoa <anflopezoc@gmail.com>
2022-01-24 22:02:26 -0500

    script

--- SprintProject-persistent/scripts/application_Start.sh
+++ SprintProject-persistent/scripts/application_Start.sh
@@ -5,5 +5,10 @@ cd /home/ubuntu/sprintproject3/SprintProject-persistent
 
 #start our node app in the background
 
+#install node modules
+sudo npm install
+echo "node modules installed"
+
+#start our node app in the background
 pm2 start ecosystem.config.js --env production --watch
 echo "run pm2"
