93c88fc23a24524ea36a0d8b93425d36b99876d1
Andrés Felipe  López Ochoa <anflopezoc@gmail.com>
2022-01-25 16:59:14 -0500

    scripts

--- SprintProject-persistent/scripts/after_Install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#navigate into our working directory where we have all our github files
-cd /home/ubuntu/sprintproject3/SprintProject-persistent
-
-#install node modules
-sudo npm install
-echo "node modules installed"
-#start our node app in the background
