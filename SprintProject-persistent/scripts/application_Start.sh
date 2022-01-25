aac7e927a959b939d5317d9c2ddbc66dbd97b78a
Andrés Felipe  López Ochoa <anflopezoc@gmail.com>
2022-01-24 22:11:42 -0500

    script

--- SprintProject-persistent/scripts/application_Start.sh
+++ SprintProject-persistent/scripts/application_Start.sh
@@ -6,7 +6,7 @@ cd /home/ubuntu/sprintproject3/SprintProject-persistent
 #start our node app in the background
 
 #install node modules
-sudo npm install
+npm install
 echo "node modules installed"
 
 #start our node app in the background
