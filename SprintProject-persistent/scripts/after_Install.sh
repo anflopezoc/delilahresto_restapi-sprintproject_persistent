25e90fa0868506c05dd6966cfc36f5ab5f7da0c6
Andrés Felipe  López Ochoa <anflopezoc@gmail.com>
2022-01-24 21:44:16 -0500

    after install

--- SprintProject-persistent/scripts/after_Install.sh
+++ SprintProject-persistent/scripts/after_Install.sh
@@ -3,7 +3,7 @@
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/sprintproject3/SprintProject-persistent
 
+#install node modules
+npm install -g 
+echo "node modules installed"
 #start our node app in the background
-
-pm2 start ecosystem.config.js --env production --watch
-echo "run pm2"
