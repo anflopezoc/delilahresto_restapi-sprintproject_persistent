e6219fbba23129d6f1ccce3de13e09611c16340a
Andrés Felipe  López Ochoa <anflopezoc@gmail.com>
2022-02-10 20:36:27 -0500

    v3

--- SprintProject-persistent/src/routes/auth/google.js
+++ SprintProject-persistent/src/routes/auth/google.js
@@ -11,14 +11,16 @@ router.get(`/${strategy_name}/callback`,
   function(req, res) {
     
     console.log(`Peticion get /${strategy_name}/callback `);
-    const data = req.user
+    /* const data = req.user
     console.log('Data:')
     console.log(data)
     const token = "hgjsd8fs6g7s7df67g6sdf43sdg2s3df5sg6s7df7"
 
     const url_front = process.env.URL_FRONT+`/?token=${token}`;
+ */
+   // res.redirect(301, url_front);
 
-    res.redirect(301, url_front);
+   res.send('Holla desde callback')
 
   });
 
