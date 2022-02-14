3d64bf60cce6aae5527dee4c57b201526bf0978d
Andrés Felipe  López Ochoa <anflopezoc@gmail.com>
2022-02-14 14:16:46 -0500

    docker pm2-runtime v1

--- SprintProject-persistent/ecosystem.config.js
+++ SprintProject-persistent/ecosystem.config.js
@@ -1,15 +1,22 @@
 module.exports = {
     apps: [{
-        name: "api",
-        script: "./src/server.js",
-        watch: true,
-        env_local:{
-            "NODE_ENV": "local",
-            "API_DESCRIPTION": "This Application is running in development mode"
+            name: "api",
+            script: "./src/server.js",
+            watch: true,
+            exec_mode: 'cluster',
+
+            env_local:{
+                "NODE_ENV": "local",
+                "API_DESCRIPTION": "This Application is running in development mode"
+            },
+            env_production:{
+                "NODE_ENV": "production",
+                "API_DESCRIPTION": "This application is running in production mode. Take care!"
+            }
         },
-        env_production:{
-            "NODE_ENV": "production",
-            "API_DESCRIPTION": "This application is running in production mode. Take care!"
+        {
+            script: 'worker.js',
+            name: 'worker'
         }
-    }]
+    ]
 };
